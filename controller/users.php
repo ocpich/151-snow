112dc616f24ba7bdcc4f0f5cfe69cf2fc6b31c81
Oceane.PICHONNAZ <Oceane.PICHONNAZ@SC-C232-PC08>
2021-11-23 10:10:04 +0100

    msg erreur

--- controller/users.php
+++ controller/users.php
@@ -13,25 +13,26 @@ require 'model/userManagement.php';
  * @brief this function is designed to display the login form
  */
 function login(){
-
     // check if email & pwd is set and if we come from login page
     if(isset($_POST['email']) && isset($_POST['userPswd'])){
 
         if(checkLogin()){
             $_SESSION['email'] = $_POST['email'];
-
             require 'view/home.php';
-            echo 'email set';
+
         }else{
+            $errorMessage = "Email incorrect";
             require 'view/login.php';
-            echo 'email not set';
         }
 
     }else{
+
         require 'view/login.php';
     }
 }
 
 function logout(){
     session_destroy();
+    $_SESSION = array();
+    require 'view/home.php';
 }
--- view/login.php
+++ view/login.php
@@ -17,10 +17,17 @@ $title = "Connectez-vous";
         <div class="row">
 
             <div class="col-md-12 p-b-30">
+
                 <form class="leave-comment" action="index.php?action=login" method="post" >
                     <h4 class="m-text26 p-b-36 p-t-15">
                         Connectez-vous
                     </h4>
+
+                    <?php
+                    if(isset($errorMessage)){
+                        echo '<h4 class="alert-danger">' . $errorMessage.'</h4>';
+                    }
+                    ?>
 
                     <div class="bo4 of-hidden size15 m-b-20">
                         <input class="sizefull s-text7 p-l-22 p-r-22" type="email" name="email" placeholder="Adresse email">
@@ -31,6 +38,8 @@ $title = "Connectez-vous";
                     </div>
                     <input type="submit" value="login" class="flex-c-m size2 bg4 bo-rad-23 hov1 m-text3 trans-0-4"><br>
                     <input type="reset" value="Annuler" class="flex-c-m size2 bg4 bo-rad-23 hov1 m-text3 trans-0-4">
+
+
 
                 </form>
             </div>
